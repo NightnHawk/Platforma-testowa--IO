903757f5ea970f017943006d4b8d6aabf09324b6
NightnHawk <grzegorz_lazinski@o2.pl>
2023-01-08 16:18:51 +0100

    Class update

--- classes/User.class.php
+++ classes/User.class.php
@@ -2,32 +2,32 @@
 
 class User{
 //properties
-protected $login;
-protected $password;
-private $type;
+	protected $login;
+	protected $password;
+	private $type;
 
 //methods
 //getters
-public function getLogin(){
-	return $this->login;
-}
-public function getPassword(){
-	return $this->password;
-}
-public function getType(){
-	return $this->type;
-}
+	public function getLogin(){
+		return $this->login;
+	}
+	public function getPassword(){
+		return $this->password;
+	}
+	public function getType(){
+		return $this->type;
+	}
 
-//setters
-public function setLogin($_login){
-	$this->login = $_login;
-}
-public function setPassword($_password){
-	$this->password = $_password;
-}
-public function setType($_type){
-	$this->type = $_type;
-}
+	//setters
+	public function setLogin($_login){
+		$this->login = $_login;
+	}
+	public function setPassword($_password){
+		$this->password = $_password;
+	}
+	public function setType($_type){
+		$this->type = $_type;
+	}
 }
 
 ?>
