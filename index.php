73e8162d811fcf333fff2f7ed73f58cb9f002062
NightnHawk <grzegorz_lazinski@o2.pl>
2023-01-08 22:43:34 +0100

    index.php graphic update
    Dodanie navigation bar

--- index.php
+++ index.php
@@ -1,39 +1,36 @@
 <?php
-include 'includes/autoloader.inc.php';
-
+	include_once "includes/autologer.inc.php";
 ?>
 <!DOCTYPE html>
-<html lang="pl" dir="ltr">
+<html lang="pl">
 <head>
-	<meta charset="uft-8">
-	<title>Strona testowa</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/main.css">
+	<title>Platforma testowa</title>
 </head>
 <body>
-<nav>
-	<div class="wrapper">
-		<a href="index.php"><img src="images\8-C1fYhy4B9Mm8wbB.png" alt="Website logo"></a>
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="includes/login.inc.php">Log in</a></li>
-		</ul>
-	</div>
-</nav>
-	<?php
-		//$testObj = new DbTest();
-		//$testObj->getUsers();
-		
-$test = new closeQuestion();
-$test->setInstruction('W którym roku wybuchła I Wojna Światowa?');
-$test->addAnswer('W roku 1918.');
-$test->addAnswer('W roku 1897.');
-
-$test->rmAnswer(0);
-
-
-echo $test->getInstruction();
-echo $test->getAnswer(1);
-
-	?>
-
+	<header class="header-main">
+		<div class="header-main-logo">
+			<img src="images\placeholder_logo.png" alt="logo">
+			<nav class="header-main-nav">
+				<ul>
+					<li><a href="index.php">Home Page</a></li>
+					<li><a href="#">User Guide</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="header-main-acount">
+			<nav class="header-main-acount-nav">
+				<ul>
+					<li><a href="includes/login.inc.php">Sign In</a></li>
+					<li><a href="#">Register</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
 </body>
 </html>
