1bc416e84daf2a542c8faad7afc373b9a8eb8479
NightnHawk <grzegorz_lazinski@o2.pl>
2023-01-08 23:31:23 +0100

    Split of index into index and header branches; new font
    Header będzie używany we wszystkich stronach, więc lepiej będzie go wyodrębnić, aby móc go później include'ować w razie potrzeby.
    Dodałęm czcionkę z fonts.google.com (Libre Basketville)

--- index.php
+++ index.php
@@ -1,36 +1,5 @@
 <?php
-	include_once "includes\autoloader.inc.php";
+	include_once "header.php";
 ?>
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/main.css">
-	<title>Platforma testowa</title>
-</head>
-<body>
-	<header class="header-main">
-		<div class="header-main-logo">
-			<img src="images\placeholder_logo.png" alt="logo">
-			<nav class="header-main-nav">
-				<ul>
-					<li><a href="index.php">Home Page</a></li>
-					<li><a href="#">User Guide</a></li>
-					<li><a href="#">About Us</a></li>
-					<li><a href="#">Contact</a></li>
-				</ul>
-			</nav>
-		</div>
-		<div class="header-main-acount">
-			<nav class="header-main-acount-nav">
-				<ul>
-					<li><a href="includes/login.inc.php">Sign In</a></li>
-					<li><a href="#">Register</a></li>
-				</ul>
-			</nav>
-		</div>
-	</header>
 </body>
 </html>
