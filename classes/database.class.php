391d8fcdaf734d4587b63aa06df0842eb9f61cb4
Okios2 <96847026+Okios2@users.noreply.github.com>
2023-01-08 16:37:26 +0100

    Połączenie z lokalną bazą

--- classes/database.class.php
+++ classes/database.class.php
@@ -7,10 +7,10 @@ class Database{
 	private $host = "localhost"; //będzia trzeba zmienić dla online hosta
 	private $user = "root";
 	private $password = "";
-	private $dbName = "UserDB";
+	private $dbName = "userdb";
 	
 	protected function connect(){
-		$dsn = "mysql:host=" . $this->host . ";dbname" . $this->dbName;
+		$dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbName;
 		$pdo = new PDO($dsn, $this->user, $this->password);
 		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 		return $pdo;
