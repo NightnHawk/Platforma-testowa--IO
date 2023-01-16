5d0da2acbd5a61fd35a887f555bb538c8cdf1acd
Okios2 <96847026+Okios2@users.noreply.github.com>
2023-01-16 19:54:55 +0100

    test

--- includes/final.inc.php
+++ includes/final.inc.php
@@ -13,7 +13,7 @@
 		<h2>The End</h2>
 			<p>You Died</p>
 			<p>Final Score: <?php echo $_SESSION['score']; ?></p>
-			<a href="test_start.inc.php" class="start">Take Again</a>
+			<a href="test_start.inc.php" class="start">Take Again<?php $_SESSION['score']=0;?></a>
 	</main>
 </body>
 </html>
