7fb1c2be6529b4eec5354a42a60bbbd8d7eee472
Okios2 <96847026+Okios2@users.noreply.github.com>
2023-01-18 22:22:24 +0100

    remove test

---
+++ includes/remove_test.inc.php
@@ -0,0 +1,54 @@
+<?php
+	include_once "../header.php";
+?>
+<?php include 'database.inc.php';?>
+<?php if(isset($_POST['submit'])){
+		//Dostać się do zmiennych w POST
+		$test_name =$_POST['test_name'];
+		
+		//test query
+		$query = "DELETE FROM tests WHERE test_name = '$test_name'";
+		//Run query
+		$remove_row = $mysqli->query($query) or die($mysqli->error._LINE_);
+		//Potwierdzenie usunięcia testu
+		if($remove_row){
+			//Choice query
+			$query = "DELETE FROM questions WHERE test_name = '$test_name'";
+			//Run query
+			$insert_row = $mysqli->query($query) or die($mysqli->error._LINE_);
+			//Choice query
+			$query = "DELETE FROM choices WHERE test_name = '$test_name'";
+			//Run query
+			$insert_row = $mysqli->query($query) or die($mysqli->error._LINE_);
+		}
+		$msg = 'Test has been removed';
+		
+	}
+	
+//Ile testów? Nie wiem, to pobiorę z bazy
+
+$query ="SELECT * FROM tests";
+$results = $mysqli->query($query) or die($mysqli->error._LINE_);
+
+?>
+
+	<main>
+		<div class="conteiner">
+			<h2>Remove Test</h1>
+			<?php 
+			if(isset($msg)){
+				echo'<p>'.$msg.'</p>';
+			}
+			?>
+			<form method="post" action="remove_test.inc.php">
+				<p>
+					<label>Test name:</label>
+					<input type="text" name="test_name"/>
+				</p>
+					<p>
+					<input type="submit" name="submit" value="submit"/>
+				</p>
+		</div>
+	</main>
+</body>
+</html>
