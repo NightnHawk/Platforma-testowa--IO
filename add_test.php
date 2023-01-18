9224bacda5c83effa2757cbbd3727b9e0361c5cc
NightnHawk <grzegorz_lazinski@o2.pl>
2023-01-18 18:21:32 +0100

    Add_test css

--- add_test.php
+++ add_test.php
@@ -1,5 +1,5 @@
 <?php
-	//include_once "../header.php";
+	include_once "header.php";
 ?>
 <?php include 'includes/database.inc.php';?>
 <?php if(isset($_POST['submit'])){
@@ -24,6 +24,7 @@
 
 	<main>
 		<div class="test_conteiner">
+			<div class="test-header">
 			<h2>Add A Test</h1>
 			<?php 
 			if(isset($msg)){
@@ -31,18 +32,19 @@
 				echo "<a href=includes/add.inc.php>Add questions to your test</a>"; 
 			}
 			?>
+			</div>
 			<form method="post" action="add_test.php">
-				<p>
+				<div class="test-textbox">
 					<label>Add a test:</label>
 					<input type="text" name="test_name"/>
-				</p>
-				<p>
+				</div>
+				<div class="test-textbox">
 					<label>Exam time:</label>
 					<input type="text" name="test_time"/>
-				</p>
-					<p>
-					<input type="submit" name="submit" value="submit"/>
-				</p>
+				</div>
+				<div class="test-button">
+					<button type="submit" name="submit">Submit</button>
+				</div>
 		</div>
 	</main>
 </body>
