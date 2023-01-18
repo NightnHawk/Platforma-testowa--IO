d72a6b09c5140f33392cff98357a5f98454ffdc8
Okios2 <96847026+Okios2@users.noreply.github.com>
2023-01-18 01:46:06 +0100

    Powrót do hubu

--- includes/final.inc.php
+++ includes/final.inc.php
@@ -1,4 +1,5 @@
 <?php session_start();?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,6 +15,15 @@
 			<p>You Died</p>
 			<p>Final Score: <?php echo $_SESSION['score']; ?></p>
 			<a href="test_start.inc.php" class="start">Take Again<?php $_SESSION['score']=0;?></a>
+			<br>
+			<?php
+			if($_SESSION["usertype"]===1){
+			echo "<a href=../hubteacher.php>Come back to the main page</a>"; 
+			}else{
+				echo "<a href=../hubstudent.php>Come back to the main page</a>"; 
+			}
+			?>
+			
 	</main>
 </body>
 </html>
