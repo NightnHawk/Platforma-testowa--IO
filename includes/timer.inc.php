732edc5abf8869c45bf746f52438e1e7584b20f9
Okios2 <96847026+Okios2@users.noreply.github.com>
2023-01-19 02:37:00 +0100

    Timer

--- includes/timer.inc.php
+++ includes/timer.inc.php
@@ -13,8 +13,7 @@ $_SESSION['start_time']=date("Y-m-d H:i:s");
 
 $end_time=date("Y-m-d H:i:s", strtotime('+'.$_SESSION['duration'].'minutes',strtotime($_SESSION['start_time'])));
 
-$_SESSION["end_time"]=$end_time;
+$_SESSION["end_time"]=$end_time;?>
 <script type="text/javasript">
-window.location="timer.inc.php";
+window.location="timer_handler.inc.php";
 </script>
-?>
